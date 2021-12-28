63ae9318fd43dfdf7765a79c01810609001532ac
Andre Santos <andresantosas2558487@gmail.com>
2021-12-28 11:42:05 -0300

    first commit

---
+++ Pessoa.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Pessoa
+{
+    // Atributos
+
+    public string nome;
+
+    public int idade;
+
+    // Métodos
+
+    public void menssagem()
+    {
+        Console.WriteLine(" Olá " +nome+ " você tem " +idade+ " anos!!");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01Conceitos
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            // Instanciando objeto
+
+            Pessoa obj = new Pessoa();
+            obj.nome = "Santiago";
+            obj.idade = 32;
+            obj.menssagem();
+        }
+    }
+}
